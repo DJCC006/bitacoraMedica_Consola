68180586020746aa1317a294babee94c67b01107
DJCC <david.carcamo2006@gmail.com>
2026-02-22 14:32:09 -0600

    init proyecto

---
+++ demostracionExpo/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <limits>
+
+using namespace std;
+
+
+struct Paciente{
+
+};
+
+
+struct Chequeo{
+
+};
+
+int main()
+{
+    cout << "Hello World!" << endl;
+    return 0;
+}
