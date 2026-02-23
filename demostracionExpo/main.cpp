40f2f8348998289769e6a0b79173cb477282c9a6
DJCC <david.carcamo2006@gmail.com>
2026-02-23 09:04:41 -0600

    agregado de detalles y funciones semifinales

--- demostracionExpo/main.cpp
+++ demostracionExpo/main.cpp
@@ -42,10 +42,13 @@ struct Paciente{
     string antMedicos;//Antecedentes medicos
 
     //vector que guarda todos los chequeos del paciente
-    //std::vector<Chequeo> expediente;
+    std::vector<Chequeo> expediente;
     int contador;
     int MAX;
-    Chequeo * expediente = new Chequeo[MAX];
+    //Chequeo * expediente = new Chequeo[MAX];
+
+
+
 };
 
 
@@ -61,18 +64,18 @@ void showExpedientes( const Paciente &paciente, int contador);
 void ordenarPacientes(Paciente* lista, int TAM);
 int buscarPorDNI(const Paciente* lista, int TAM, int IDBuscado);
 void cerrarExpedientes(Paciente* lista, int contador);
-int buscarExpediente(const Chequeo* expediente, int TAM, int code);
-void showInfoChequeo(const Chequeo* expediente, int index);
+int buscarExpediente(std::vector<Chequeo> expediente, int TAM, int code);
+void showInfoChequeo(std::vector<Chequeo> expediente, int index);
 void previewPacientes(const Paciente* lista,int contador);
 
 int main()
 {
     SetConsoleOutputCP(65001);
-    int TAM=8;
     //STACK
-    Paciente* listaPacientes= new Paciente[TAM];
+    int TAM=8;
     int contador=0;
     int opcion=0;
+    Paciente* listaPacientes= new Paciente[TAM];
 
     do{
         showMenuPrincipal();
@@ -91,43 +94,82 @@ int main()
                  * mandamos info
                  *
                  * */
-                int dniBuscado=0;
-                cout<<"\nIngrese el DNI del paciente a realizar chequeo: ";
-                cin>>dniBuscado;
-                int index=buscarPorDNI(listaPacientes, TAM, dniBuscado);
-                crearChequeo(listaPacientes, index);
+                if(contador>0){
+                    int dniBuscado=validarEntero("\nIngrese el DNI del paciente a realizar chequeo: ");
+                    int index=0;
+                    index=buscarPorDNI(listaPacientes, TAM, dniBuscado);
+                    if(index!=-1){
+                        crearChequeo(listaPacientes, index);
+                    }else{
+                        cout<<"\n>>ERROR: No se encontró paciente"<<endl;
+                    }
+
+                }else{
+                    cout<<"\n>>AVISO: No hay pacientes registrados."<<endl;
+                }
+
 
                 break;
             }
 
             case 3:{
-                listarPacientes(listaPacientes, contador);
+                if(contador>0){
+                    listarPacientes(listaPacientes, contador);
+                }else{
+                    cout<<"\nAVISO: No hay pacientes registrados."<<endl;
+                }
+
                 break;
             }
 
             case 4:{
-                int dniBuscado=0;
-                int expBuscado=0;
-                previewPacientes(listaPacientes, contador);
-                cout<<"\nIngrese el DNI de paciente: ";
-                cin>>dniBuscado;
-                int index = buscarPorDNI(listaPacientes, TAM, dniBuscado);
-                cout<<"index: "<<index<<endl;
-                Paciente pass = listaPacientes[index];
-                showExpedientes(pass, pass.contador);
+                if(contador>0){
+                    int dniBuscado=0;
+                    int expBuscado=0;
+                    previewPacientes(listaPacientes, contador);
+                    dniBuscado= validarEntero("\nIngrese el DNI de paciente: ");
+                    if(dniBuscado!=-1){
+                        int index=0;
+                        index = buscarPorDNI(listaPacientes, TAM, dniBuscado);
+                        if(index!=-1){
+                            Paciente pass = listaPacientes[index];
+                            showExpedientes(pass, pass.contador);
 
-                expBuscado=validarEntero("Ingrese el codigo del expediente a visualizar: ");
-                int indexE= buscarExpediente(pass.expediente,pass.contador, expBuscado);
-                showInfoChequeo(pass.expediente,indexE);
+                            expBuscado=validarEntero("Ingrese el codigo del expediente a visualizar: ");
+                            int indexE=0;
+                            indexE= buscarExpediente(pass.expediente,pass.contador, expBuscado);
+                            if(indexE!=-1){
+
+                                showInfoChequeo(pass.expediente,indexE);
+                            }else{
+                                cout<<">>\nERROR: No se encontró expediente."<<endl;
+                            }
+                        }else{
+                            cout<<">>\nERROR: No se encontró paciente."<<endl;
+                        }
+
+
+                    }else{
+                        cout<<"\n>>ERROR: No se encontró paciente."<<endl;
+                    }
+
+                }else{
+                    cout<<"\n>>AVISO: No hay pacientes registrados."<<endl;
+                }
+
                 break;
             }
 
             case 5:{
                 cout<<"\nCerrando Sesion..."<<endl;
-                cerrarExpedientes(listaPacientes, contador);
+                //cerrarExpedientes(listaPacientes, contador);
                 delete[] listaPacientes;
                 listaPacientes=nullptr;
                 break;
+            }
+
+            default:{
+                cout<<"\n>>AVISO: Ingrese una opcion valida."<<endl;
             }
         }
 
@@ -218,10 +260,12 @@ void crearChequeo(Paciente* lista, int index){
 
 
     //Registramos codigo de chequeo
-    //pacienteAdd.expediente.push_back(newCheck);
-    //int idChequeo= pacienteAdd.expediente.size();
+
     newCheck.idExpediente=pacienteAdd.contador+1;
-    pacienteAdd.expediente[pacienteAdd.contador]=newCheck;
+    pacienteAdd.expediente.push_back(newCheck);
+    //int idChequeo= pacienteAdd.expediente.size();
+
+    //pacienteAdd.expediente[pacienteAdd.contador]=newCheck;
     pacienteAdd.contador++;
     //agregamos el nuevo chequeo al expediente del paciente
 
@@ -252,8 +296,9 @@ void listarPacientes(const Paciente* lista,int contador){
 
 
     //heading
-    cout<<"\nDNI         NOMBRE      APELLIDO      SEXO      EDAD      TIPO SANGRE      TELEFONO"<<endl;
-    cout<<"-------------------------------------------------------------------------------------"<<endl;
+    cout<<"\n------------------------------PACIENTES REGISTRADOS-------------------------------------"<<endl;
+    cout<<"DNI         NOMBRE      APELLIDO         SEXO      EDAD      TIPO SANGRE      TELEFONO"<<endl;
+    cout<<"----------------------------------------------------------------------------------------"<<endl;
 
     //iteracion
     for(int i=0; i<contador; i++){
@@ -265,7 +310,7 @@ void listarPacientes(const Paciente* lista,int contador){
         cout<<setw(10)<<lista[i].edad;
         cout<<setw(10)<<lista[i].typeBlood;
         cout<<setw(21)<<lista[i].telefono<<endl;
-        cout<<"-------------------------------------------------------------------------------------"<<endl;
+        cout<<"-----------------------------------------------------------------------------------------"<<endl;
     }
 }
 
@@ -277,11 +322,11 @@ void showExpedientes(const Paciente &paciente, int contador){
 
    // cout<<"total: "<<totalExpediente<<endl;
     //heading
-    cout<<"------EXPEDIENTE DE PACIENTE-----"<<endl;
-    cout<<"\nCODIGO      FECHA        MOTIVO      "<<endl;
+    cout<<"\n------EXPEDIENTE DE PACIENTE-----"<<endl;
+    cout<<"CODIGO      FECHA        MOTIVO      "<<endl;
     cout<<"----------------------------------------"<<endl;
     for(int i=0; i<paciente.contador; i++){
-        Chequeo check= paciente.expediente[i];
+        Chequeo check= paciente.expediente.at(i);
         cout<<check.idExpediente;
         cout<<setw(18)<<check.fecha;
         cout<<setw(20)<<check.motConsulta<<endl;
@@ -329,9 +374,9 @@ int buscarPorDNI(const Paciente* lista, int TAM, int IDBuscado){
 
 
 
-int buscarExpediente(const Chequeo* expediente, int TAM, int code){
+int buscarExpediente(std::vector<Chequeo> expediente, int TAM, int code){
     for(int i=0; i<TAM; i++){
-        if(expediente[i].idExpediente==code){
+        if(expediente.at(i).idExpediente==code){
             return i;
         }
     }
@@ -342,7 +387,8 @@ int buscarExpediente(const Chequeo* expediente, int TAM, int code){
 
 void previewPacientes(const Paciente* lista,int contador){
     //heading
-    cout<<"\nDNI         NOMBRE      APELLIDO"<<endl;
+    cout<<"\n----------LISTA PACIENTES----------"<<endl;
+    cout<<"DNI         NOMBRE      APELLIDO"<<endl;
     cout<<"-----------------------------------"<<endl;
 
     //iteracion
@@ -355,7 +401,7 @@ void previewPacientes(const Paciente* lista,int contador){
 }
 
 
-void showInfoChequeo(const Chequeo* expediente, int index){
+void showInfoChequeo(std::vector<Chequeo> expediente, int index){
     cout<<"\n---DETALLES DE EXPEDIENTE---"<<endl;
     cout<<"\n--Registro de Signos Vitales--"<<endl;
     cout<<"Presión Arterial: "<<expediente[index].presionA<<" mmHg"<<endl;
@@ -374,7 +420,7 @@ void showInfoChequeo(const Chequeo* expediente, int index){
 //Metodo utilizado para cerrar los recursos de los arreglos de expedientes para cada paciente
 void cerrarExpedientes(Paciente* lista, int contador){
     for(int i=0; i<contador; i++){
-        delete[] lista[i].expediente;
-        lista[i].expediente=nullptr;
+        //delete lista[i].expediente;
+        //lista[i].expediente=nullptr;
     }
 }
